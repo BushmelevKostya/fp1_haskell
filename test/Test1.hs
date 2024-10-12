2f68053384ad72b432c3839ce580dca28d22c4ed
Kostya <kostikbushmelev@gmail.com>
2024-10-12 11:48:47 +0300

    upd tests

--- test/Test1.hs
+++ test/Test1.hs
@@ -8,6 +8,7 @@ import Task3
 import Task4
 import Task5
 import Task6
+import Utils (readNumber)
 
 tests :: TestTree
 tests = testGroup "Test1" [testTask1To6]
@@ -20,11 +21,11 @@ expectedResult = 141087744000
 testTask1To6 :: TestTree
 testTask1To6 =
   testCase "Testing Task1 to Task6" $ do
-    let result1 = return recursion
-    let result2 = return tailRecursion
-    let result3 = return mapResult reduce
-    let result4 = return mapSequence
-    let result5 = return listComprehensions
-    let result6 = return infList
+    let result1 = return recursion readNumber
+    let result2 = return tailRecursion readNumber
+    let result3 = return mapResult reduce readNumber
+    let result4 = return mapSequence readNumber
+    let result5 = return listComprehensions readNumber
+    let result6 = return infList readNumber
     let totalResult = result1 + result2 + result3 + result4 + result5 + result6
     totalResult @?= expectedResult
