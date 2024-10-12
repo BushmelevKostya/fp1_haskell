575b03d85a87555c8ce13a59603db5f187713695
Kostya <kostikbushmelev@gmail.com>
2024-10-12 08:43:59 +0300

    Upd ci.yml

--- app/Main.hs
+++ app/Main.hs
@@ -18,31 +18,31 @@ main :: IO()
 main = do
     --8 task
     digits <- readNumber
-    putStrLn ("Task's 1 recursion result is:")
+    putStrLn "Task's 1 recursion result is:"
     print $ recursion digits
-    putStrLn ("Task's 2 tailRecursion result is:")
+    putStrLn "Task's 2 tailRecursion result is:"
     print $ tailRecursion digits
-    putStrLn ("Task's 3 mapResult result is:")
+    putStrLn "Task's 3 mapResult result is:"
     print $ mapResult . reduce $ digits
-    putStrLn ("Task's 4 mapSequence result is:")
+    putStrLn "Task's 4 mapSequence result is:"
     print $ mapSequence digits
-    putStrLn ("Task's 5 listComprehensions result is:")
+    putStrLn "Task's 5 listComprehensions result is:"
     print $ listComprehensions digits
-    putStrLn ("Task's 6 infList result is:")
+    putStrLn "Task's 6 infList result is:"
     print $ infList digits
-    
-    putStrLn("")
+
+    putStrLn ""
 
     --23 task
-    putStrLn ("Task's 7 recursion result is:")
-    print $ recursion2
-    putStrLn ("Task's 8 tailRecursion result is:")
-    print $ tailRecursion2
-    putStrLn ("Task's 9 mapResult result is:")
-    print $ moduleSum
-    putStrLn ("Task's 10 mapSequence result is:")
-    print $ mapSequence2
-    putStrLn ("Task's 11 listComprehensions result is:")
-    print $ listComprehensions2
-    putStrLn ("Task's 12 infList result is:")
-    print $ infList2
+    putStrLn "Task's 7 recursion result is:"
+    print recursion2
+    putStrLn "Task's 8 tailRecursion result is:"
+    print tailRecursion2
+    putStrLn "Task's 9 mapResult result is:"
+    print moduleSum
+    putStrLn "Task's 10 mapSequence result is:"
+    print mapSequence2
+    putStrLn "Task's 11 listComprehensions result is:"
+    print listComprehensions2
+    putStrLn "Task's 12 infList result is:"
+    print infList2
